1690571eee392be2ec9c96c61528fa375469a7f7
Khushman Patel <khushman1@gmail.com>
2025-03-01 23:25:46 -0800

    Updated svelte config with base path

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,7 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		paths: {
-			base: process.argv.includes('dev') ? '' : 'date-delights'
+			base: process.argv.includes('dev') ? '' : '/date-delights'
 		}
 	}
 };
