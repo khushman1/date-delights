ebfb6e483b4b6d3d72b7829c5ca58e62a259e16f
Khushman Patel <khushman1@gmail.com>
2025-03-02 20:09:13 -0800

    Rebuild with ios fix

---
+++ docs/_app/immutable/chunks/DKIoaWBk.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1tmckn2)==null?void 0:s.base)??"/date-delights";var t;const a=((t=globalThis.__sveltekit_1tmckn2)==null?void 0:t.assets)??e;export{a,e as b};
